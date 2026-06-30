2fcf810e4112df12240a649c5ebbd67809cc251d
vanshikachauhan18 <vanshikachauhan1811@gmail.com>
2026-06-30 09:52:38 +0530

    router-changes

--- react-router/src/component/Navbar.jsx
+++ react-router/src/component/Navbar.jsx
@@ -1,7 +1,12 @@
+import {Link} from 'react-router-dom';
+
 function Navbar(){
 
   return(
     <>
+    <Link to = '/'>Home</Link>
+    <Link to = '/about'>About us</Link>
+    <Link to = '/about/contact'>Contact</Link>
     <h1>This is the navbar</h1>
     </>
   )
--- react-router/src/pages/About.jsx
+++ react-router/src/pages/About.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function About(){
+  const navigate = useNavigate();
+
 
   return(
     <>
     <h1>This is the About page</h1>
+    <button onClick={() => navigate('/')}>Go back to home page</button>
+    <button onClick={() => navigate('/about/contact')}>Go back to contact page</button>
     </>
   )
 }
