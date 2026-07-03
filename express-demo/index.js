7463ea3594567c6eb9796e276c11d58d64678bc1
vanshikachauhan18 <vanshikachauhan1811@gmail.com>
2026-07-03 10:22:11 +0530

    added backed

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,14 +2,21 @@ import express from 'express';
 
 const app = express();
 
+app.use((req,res,next) => {
+    console.log("running");
+    next();
+})
 app.get('/',((req,res) =>{
     res.send("<h1>Hello World</h1>");
     
 }));
 app.get('/student',((req,res) =>{
-    
-    res.json({name :"abc",city:"city",age:20})
+    res.json({id :1, name :"abc",city:"city",age:20})
 }));
+
+app.get("/profile", (req, res) => {
+  res.status(200).send("this is a profile page");
+});
 
 app.listen(3000, () =>{
     console.log("Server is running on the port");
