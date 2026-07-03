52c09a76c06239db5a83d98dbfacf7c7e9c9ab78
vanshikachauhan18 <vanshikachauhan1811@gmail.com>
2026-07-03 11:08:41 +0530

    added api

---
+++ crud-api/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+const students = [{
+    id:1,
+    name:"abc",
+    class:9,
+    city:"mumbai",
+    school:"xyz international school"
+},
+{
+    id:2,
+    name:"hen",
+    class:10,
+    city:"delhi",
+    school:"xyz international school"
+}
+];
+
+app.get("/students",(req,res) =>{
+    res.json(students);
+})
+app.post("students",(req,res) =>{
+    students.push({id:3,name:"newname",age:12,city:"kolkata",school:"xyz international school"});
+    res.json({
+        message:"Student added sucessfully",students
+    })
+})
+app.listen(3000, () =>{
+    console.log("Server is running on the port");
+})
