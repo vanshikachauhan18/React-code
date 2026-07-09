3ea809f2f67b311e59dbe28cf7bb74894292da31
vanshikachauhan18 <vanshikachauhan1811@gmail.com>
2026-07-09 10:51:52 +0530

    Change MongoDB
    Updated MongoDB connection string to an empty value.

--- mongodb-demo/config/db.js
+++ mongodb-demo/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://vanshika:<id_pw>@cluster0.fm7difs.mongodb.net/?appName=Cluster0");
+        await mongoose.connect("");
         console.log("MongoDB connected");
     } catch (err) {
         console.error("MongoDB connection error:", err);
